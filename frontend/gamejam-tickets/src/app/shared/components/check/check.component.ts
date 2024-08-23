422c6182f110d78d6e49b0d9aaf2d9d02c46d275
tim-scarlith <anthony00052023@gmail.com>
2024-08-23 16:57:24 -0600

    GTP-21
    Creacion del componente check falta acomodarlo en dentro del ticket-box

---
+++ frontend/gamejam-tickets/src/app/shared/components/check/check.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-check',
+  standalone: true,
+  imports: [],
+  templateUrl: './check.component.html',
+  styleUrl: './check.component.css'
+})
+export class CheckComponent {
+
+}
