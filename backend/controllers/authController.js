8796c96abc9645aa05bae37ed3e2c2f170e63b37
Gravehg <80481681+Gravehg@users.noreply.github.com>
2024-08-23 14:18:31 -0600

    GTP-20 Added login formulary and some backend logic
    Added frontend initial formulary and backend logic for session

---
+++ backend/controllers/authController.js
@@ -0,0 +1,104 @@
+const JWT = require("jsonwebtoken");
+const User = require("../models/userModel");
+const { sendEmail } = require("../services/mailer");
+
+const login = async (req, res) => {
+  try {
+    const decodedToken = JWT.verify(req.params.token, process.env.JWT_SIGN);
+    const userId = decodedToken.userId;
+    const roles = decodedToken.roles;
+    const email = decodedToken.email;
+
+    const loginToken = JWT.sign({ userId, roles, email }, process.env.JWT_SIGN);
+
+    const rolesToCheck = [
+      "LocalOrganizer",
+      "GlobalOrganizer",
+      "Judge",
+      "Jammer",
+      "Support",
+    ];
+
+    const hasAnyRole = rolesToCheck.some((role) => roles.includes(role));
+
+    if (!hasAnyRole) {
+      return res.redirect(`http://${process.env.URL}:${process.env.APP_PORT}/`); //Should send to login
+    }
+
+    res.cookie("session-token", loginToken, {
+      httpOnly: false,
+      sameSite: "lax",
+      maxAge: 1000 * 60 * 30,
+    });
+
+    if (roles.includes("GlobalOrganizer")) {
+      return res.redirect(
+        `http://${process.env.URL}:${process.env.APP_PORT}/ticket-pool`
+      );
+    } else if (roles.includes("Support")) {
+      return res.redirect(
+        `http://${process.env.URL}:${process.env.APP_PORT}/ticket-pool`
+      );
+    } else {
+      return res.redirect(
+        `http://${process.env.URL}:${process.env.APP_PORT}/my-tickets`
+      );
+    }
+  } catch (error) {
+    return res
+      .clearCookie("session-token")
+      .redirect(`http://${process.env.URL}/login/error`);
+  }
+};
+
+const magicLink = async (req, res) => {
+  const email = req.body.email;
+  const user = await User.findOne({ email });
+
+  if (!user)
+    res
+      .status(401)
+      .json({ success: false, msg: "You must be registered first!" });
+
+  const token = JWT.sign(
+    { userId: user._id, roles: user.roles, email: user.email },
+    process.env.JWT_SIGN,
+    {
+      expiresIn: 3600,
+    }
+  );
+  const link = `http://${process.env.URL}:${process.env.APP_PORT}/api/auth/login/${token}`;
+  const subject = "Login in GameJam Support Platform";
+  const message = `Hi, click on this link to continue to the app:`;
+  await sendEmail(email, subject, message, link);
+  res.status(200).json({
+    success: true,
+    msg: `Magic Link sent to user's email`,
+    email,
+    magicLink,
+  });
+};
+
+// Esto lo estoy probando, es para verificar que exite una session abierta y
+// salta el login
+const verifyToken = async (req, res) => {
+  const token = req.cookies.sessionToken;
+  if (!token) {
+    return res.status(401).json({ error: "No token provided" });
+  }
+
+  try {
+    const decoded = JWT.verify(token, process.env.ACCESS_TOKEN_SEQUENCE);
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Token is valid", userId: user._id });
+  } catch (error) {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+};
+
+module.exports = { login, verifyToken, magicLink };
--- frontend/gamejam-tickets/src/app/app.routes.ts
+++ frontend/gamejam-tickets/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  {
+    path: '**',
+    component: LoginComponent,
+  },
+];
