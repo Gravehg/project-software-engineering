b1116dd344264ec843a3cd2b099a7b5a2e866d8d
Gravehg <80481681+Gravehg@users.noreply.github.com>
2024-08-23 18:47:32 -0600

    GTP-20 Added form control to login

--- frontend/gamejam-tickets/src/app/login/login.component.ts
+++ frontend/gamejam-tickets/src/app/login/login.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private fb: FormBuilder = inject(FormBuilder);
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+  });
 
+  constructor() {}
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
 }
