fc74947ea934f70aed3285723e46b2b9ba0acfed
Gravehg <80481681+Gravehg@users.noreply.github.com>
2024-08-23 20:15:49 -0600

    GTP-20 Added translation for login page

--- frontend/gamejam-tickets/src/app/app.config.ts
+++ frontend/gamejam-tickets/src/app/app.config.ts
@@ -9,9 +9,8 @@ import { routes } from './app.routes';
 import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
 export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, './i18n/', '.json');
+  return new TranslateHttpLoader(http, './i18n/', '.json?v=' + Date.now());
 }
 
 export const appConfig: ApplicationConfig = {
--- frontend/gamejam-tickets/src/app/login/login.component.ts
+++ frontend/gamejam-tickets/src/app/login/login.component.ts
@@ -6,16 +6,18 @@ import {
   FormBuilder,
   Validators,
 } from '@angular/forms';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, TranslateModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   private fb: FormBuilder = inject(FormBuilder);
+  translate: TranslateService = inject(TranslateService);
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
   });
@@ -26,5 +28,13 @@ export class LoginComponent {
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
     }
+  }
+
+  onLanguageChange(event: Event): void {
+    const selectedLanguage = (event.target as HTMLSelectElement).value;
+    this.translateText(selectedLanguage);
+  }
+  translateText(lang: string) {
+    this.translate.use(lang);
   }
 }
