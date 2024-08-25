221b34e9394a27590dd96b45474bf83e2d9f69cb
tim-scarlith <anthony00052023@gmail.com>
2024-08-24 18:53:03 -0600

    GTP-21
    Se creo la selección única de categoría para el ticket a nivel de interfaz grafica.

--- frontend/gamejam-tickets/src/app/shared/components/check/check.component.ts
+++ frontend/gamejam-tickets/src/app/shared/components/check/check.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Asegúrate de importar CommonModule
 
 @Component({
   selector: 'app-check',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // Añadir CommonModule aquí
   templateUrl: './check.component.html',
-  styleUrl: './check.component.css'
+  styleUrls: ['./check.component.css']
 })
 export class CheckComponent {
+  // Array para almacenar los checkboxes
+  checkboxes = [
+    { label: 'Tecnología', checked: false },
+    { label: 'Eventos', checked: false },
+    { label: 'Aceleración', checked: false },
+    { label: 'Viajes', checked: false },
+    { label: 'Fellows', checked: false }
+  ];
 
+  // Función para seleccionar solo un checkbox
+  selectOnlyOne(index: number): void {
+    this.checkboxes.forEach((checkbox, i) => {
+      checkbox.checked = i === index;
+    });
+  }
 }
