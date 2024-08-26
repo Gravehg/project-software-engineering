2dd85ed327cd9128845020e1f3062ac5584cfcbc
Josue Torres <jtn1999@estudiantec.cr>
2024-08-26 10:12:04 -0600

    GTP-25
    Termine la interfaz gráfica, ahora tengo que finalizar el backend

--- frontend/gamejam-tickets/src/app/jammers-users/tickets-jammers/tickets-jammers.component.ts
+++ frontend/gamejam-tickets/src/app/jammers-users/tickets-jammers/tickets-jammers.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarJammerComponent } from '../../shared/components/nav-bar-jammer/nav-bar-jammer.component';
+import { CardComponent } from '../../shared/components/card/card.component';
 
 @Component({
   selector: 'app-tickets-jammers',
   standalone: true,
-  imports: [NavBarJammerComponent],
+  imports: [NavBarJammerComponent, CardComponent],
   templateUrl: './tickets-jammers.component.html',
   styleUrl: './tickets-jammers.component.css'
 })
