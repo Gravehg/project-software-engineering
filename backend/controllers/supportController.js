27b237c742b4784fed865474d16a3ccb99e2e176
Gravehg <80481681+Gravehg@users.noreply.github.com>
2024-08-28 22:32:10 -0600

    GTP-53 Added route to get support categories

--- backend/controllers/supportController.js
+++ backend/controllers/supportController.js
@@ -1,6 +1,21 @@
+const SupportCategory = require("./supportCategoryController");
+
 const getAssignedTickets = (req, res) => {
   const userPayLoad = req.userPayLoad;
   const supportPayLoad = userPayLoad.supportInfo;
+  console.log(supportPayLoad);
+  return res.status(200).json({ success: true, msg: "Testing" });
 };
 
-module.exports = { getAssignedTickets };
+const getSupportCategories = (req, res) => {
+  const userPayLoad = req.userPayLoad;
+  const supportPayLoad = userPayLoad.supportInfo;
+  const supportCategories = supportPayLoad.supportCategories;
+  const r_categories = supportCategories.map((category) => {
+    const { __v, ...rest } = category.toObject();
+    return rest;
+  });
+  return res.status(201).json(r_categories);
+};
+
+module.exports = { getAssignedTickets, getSupportCategories };
