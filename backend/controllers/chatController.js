532a97629afbb96e076270968ba831208994ff0b
atherion-wq <67752809+atherion-wq@users.noreply.github.com>
2024-09-26 17:13:15 -0600

    GTP-62
    Delete testing function

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -17,22 +17,6 @@ const getMessages = async (req, res) => {
   }
 };
 
-const getMessages2 = async (req, res) => {
-  try {
-    const chatID = req.query.chatID;
-    const messages = await Message.find({
-      idChat: chatID,
-    })
-      .sort({ textDate: 1 })
-      .exec();
-    return res.status(200).json(messages);
-  } catch {
-    return res
-      .status(500)
-      .json({ success: false, msg: "There has been an error" });
-  }
-};
-
 
 
 const getChatID = async (req, res) => {
